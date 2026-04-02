cb99c08ae60fef29023f33a34feda0a750171fc2
anjana oshadha <aoshadha00@gmail.com>
2026-04-02 18:44:38 +0530

    replace with minimal working timetable backend

--- controllers/timetableController.js
+++ controllers/timetableController.js
@@ -1,27 +1,16 @@
 const getTimetable = (req, res) => {
-  const timetable = [
-    {
-      id: 1,
-      busName: "Bus 101",
-      route: "Colombo - Negombo",
-      departureTime: "08:00 AM",
-      arrivalTime: "09:30 AM"
-    },
-    {
-      id: 2,
-      busName: "Bus 202",
-      route: "Negombo - Colombo",
-      departureTime: "10:00 AM",
-      arrivalTime: "11:30 AM"
-    }
-  ];
-
   res.status(200).json({
     success: true,
-    data: timetable
+    data: [
+      {
+        id: 1,
+        busName: "Bus 101",
+        route: "Colombo - Negombo",
+        departureTime: "08:00 AM",
+        arrivalTime: "09:30 AM"
+      }
+    ]
   });
 };
 
-module.exports = {
-  getTimetable
-};
+module.exports = { getTimetable };
