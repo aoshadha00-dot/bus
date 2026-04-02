d3c069ecdeff557ecca6578abc999ec925870f9b
anjana oshadha <aoshadha00@gmail.com>
2026-04-02 18:34:50 +0530

    fix timetable controller filename

--- controllers/timetableController.js
+++ controllers/timetableController.js
@@ -1,16 +1,5 @@
 const getTimetable = (req, res) => {
-  const data = [
-    {
-      id: 1,
-      bus: "Bus 101",
-      route: "Colombo - Negombo",
-      time: "08:00 AM"
-    }
-  ];
-
-  res.json(data);
+  res.json({ message: "working" });
 };
 
-module.exports = {
-  getTimetable
-};
+module.exports = { getTimetable };
